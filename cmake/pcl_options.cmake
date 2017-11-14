4148ccc2ea11c81f574ddc026d8fabfc4a262b00
Levi Armstrong <levi.armstrong@gmail.com>
2017-11-14 00:00:49 -0600

    Move advancing front debug tool

--- cmake/pcl_options.cmake
+++ cmake/pcl_options.cmake
@@ -55,6 +55,4 @@ endif(USE_PROJECT_FOLDERS)
 
 option(BUILD_tools "Useful PCL-based command line tools" ON)
 
-option(BUILD_surface_afront_debug "Enable afront mesher debug interface" OFF)
-
 option(WITH_DOCS "Build doxygen documentation" OFF)
